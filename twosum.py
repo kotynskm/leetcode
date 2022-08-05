3fc9cad45d9ffdd5708933f6de6e0c33763db466
Kailey <k.kotynski@gmail.com>
2022-08-05 11:58:07 -0700

    Added additional solution to twosum.py

--- twosum.py
+++ twosum.py
@@ -12,3 +12,19 @@ def twoSum(nums, target):
             # does sum of num[i] and num[j] == target? if so, return i and j  
             if nums[i] + nums[j] == target:
                 return i,j
+
+# solution with enumerate
+
+def twoSum(nums, target):
+    # create a hash table to store nums in array
+    num_dict = {}
+        
+    # loop with enumerate
+    for i,num in enumerate(nums):
+        # calc num needed
+        num_needed = target - num
+        if num_needed in num_dict:
+            return [i, nums.index(num_needed)]
+        else:
+            num_dict[num] = True
+    return []
