0a5de877a1490ea20cf5390b678f9f74d50d0131
Kailey <k.kotynski@gmail.com>
2022-08-29 15:50:34 -0700

    Added solution for topkfreq.py

---
+++ topkfreq.py
@@ -0,0 +1,24 @@
+""" Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order. """
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        result = []
+        count_map = {}
+        # creates a freq array, with arrays at each index all the way to the length of the input
+        freq = [[] for i in range(len(nums) + 1)]
+        
+        # populate hash table with nums and counts
+        for n in nums:
+            count_map[n] = 1 + count_map.get(n, 0)
+            
+        # populate freq table
+        for number, count in count_map.items():
+            freq[count].append(number)
+            
+        # loop over freq table starting from the back, because that's the highest count
+        for i in range(len(freq) - 1, 0, -1):
+            # there may be multiple nums at that frequency, so must loop over them
+            for num in freq[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
