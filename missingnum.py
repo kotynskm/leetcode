bd7091facc4b1c360f914c547712cbb45679a33b
Kailey <k.kotynski@gmail.com>
2022-09-01 15:42:53 -0700

    Added additional solution for missingnum.py

--- missingnum.py
+++ missingnum.py
@@ -12,3 +12,14 @@ class Solution:
         for num in nums_table:
             if num not in nums:
                 return num
+
+# solution using O(1) space
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        length = len(nums)
+        
+        # calc the sum expected and the sum actual
+        sumExp = length * (length + 1)//2
+        sumActual = sum(nums)
+        
+        return sumExp - sumActual
