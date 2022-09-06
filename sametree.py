eb291f4fe28f45e265be493c88c26fe1d92d1114
Kailey <k.kotynski@gmail.com>
2022-09-06 15:48:09 -0700

    Added additional solution for sametree.py

--- sametree.py
+++ sametree.py
@@ -21,3 +21,31 @@ class Solution:
             return False
         
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    # iterative solution with BFS and queue
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # BFS with queue
+        que = [(p, q)]
+        
+        while que:
+            # pop nodes
+            node1, node2 = que.pop(0)
+            # if both are None
+            if not node1 and not node2:
+                return True
+            # if only one is None
+            if not node1 or not node2:
+                return False
+            # if the values are not equal
+            if node1.val != node2.val:
+                return False
+            # append left and right nodes
+            que.append((node1.left, node2.left))
+            que.append((node1.right, node2.right))  
