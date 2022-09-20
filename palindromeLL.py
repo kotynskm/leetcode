b2edb020a907d41d19e3586a81d7becd6282d937
Kailey <k.kotynski@gmail.com>
2022-09-20 13:20:11 -0700

    Added additional solution for palindromeLL.py

--- palindromeLL.py
+++ palindromeLL.py
@@ -26,3 +26,36 @@ class Solution:
             l += 1
             r -= 1
         return True
+
+    # solution that doesn't use extra space O(1)
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # find the middle of the linked list
+        fast, slow = head, head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        # reverse the middle
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        # check if palindrome using two pointers
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            else:
+                left = left.next
+                right = right.next
+        return True
