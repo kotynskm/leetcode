ee603911d27590b98e3d217a95f29d952f8b87d2
Kailey <k.kotynski@gmail.com>
2022-09-28 14:46:59 -0700

    Added solution for containsdupII.py

---
+++ containsdupII.py
@@ -0,0 +1,17 @@
+""" Given an integer array nums and an integer k, return true if there are two distinct indices i and j 
+
+in the array such that nums[i] == nums[j] and abs(i - j) <= k. """
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hash = {}
+        
+        for i, num in enumerate(nums):
+            if num not in hash:
+                hash[num] = i
+            else:
+                if abs(i - hash[num]) <= k:
+                    return True
+                else:
+                    hash[num] = i
+        return False
