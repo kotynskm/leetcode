37477b9185a9eaa46c97c7718194d27c572eb37e
Kailey <k.kotynski@gmail.com>
2022-11-01 16:23:00 -0700

    Added additional solution to postorder.py

--- postorder.py
+++ postorder.py
@@ -22,3 +22,33 @@ class Solution:
             return res
         
         return dfs(root)
+
+# iterative solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # post order is visit left, then right, then node
+        res = []
+        visited = []
+        
+        stack = [root]
+        
+        while stack:
+            node = stack.pop()
+            if node:
+                visited.append(node.val)
+
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+
+        while visited:
+            res.append(visited.pop())
+            
+        return res
