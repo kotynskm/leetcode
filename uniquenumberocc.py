a8e4f6c429a1210e54b29141166d45749f8b6443
Kailey <k.kotynski@gmail.com>
2023-02-09 14:40:14 -0800

    Added additional solutiong to uniquenumberocc.py

--- uniquenumberocc.py
+++ uniquenumberocc.py
@@ -12,4 +12,21 @@ class Solution:
             num_set.add(value)
 
         return len(num_set) == len(hash)
+
+# We can optimize by adding values if not already in the set - we can return early if we see a repeat number
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        hash = {}
+        num_set = set()
+
+        for num in arr:
+            hash[num] = hash.get(num, 0) + 1
+
+        for value in hash.values():
+            if value not in num_set:
+                num_set.add(value)
+            else:
+                return False
+
+        return True
         
